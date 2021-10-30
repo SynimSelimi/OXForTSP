f1d90748a84762e330e3dbefa5ab8941bb850706
SynimSelimi <26981059+SynimSelimi@users.noreply.github.com>
2021-10-30 15:37:32 +0200

    Refactoring and fixes

--- main.py
+++ main.py
@@ -3,39 +3,41 @@
 import random
 import collections
 
+def crossover(p1, p2):
+    c1 = [0] * len(p1)
+    
+    numbers = random.sample(range(1, len(p1) - 1), 2)
+    numbers.sort()
+    [a, b] = numbers
+    # [a, b] = [2, 4]
+
+    for e in range(a, b + 1):
+        c1[e] = p1[e]
+        
+    remaining = list(set(p2) - set(c1))
+
+    p2_r = collections.deque(p2)
+    p2_r.rotate(len(p2) - b - 1)
+    p2_r = list(p2_r)
+    print("cutoff", f"{a}:{b}", p1[a:b+1], b - a)
+    print("remaining", remaining)
+    
+    i = b + 1
+    for el in p2_r:
+        if el in remaining:
+            c1[i] = el
+            end_of_array = i == len(p2) - 1
+            i = 0 if end_of_array else i + 1
+            
+    return c1
+
 p1 = [7, 3, 1, 8, 2, 4, 6, 5]
 p2 = [4, 3, 2, 8, 6, 7, 1, 5]
 
-c1 = [0] * len(p1)
+# p1 = [3, 4, 8, 2, 7, 1, 6, 5]
+# p2 = [4, 2, 5, 1, 6, 8, 3, 7]
 
-numbers = random.sample(range(1, len(p1) - 1), 2)
-numbers.sort()
-[a, b] = numbers
-
-print("cutoff", numbers)
-
-for e in range(a, b + 1):
-    c1[e] = p1[e]
-
-for e in range(a, b + 1):
-    c1[e] = p1[e]
-    
-remaining = list(set(p2) - set(c1))
-
-p2_r = collections.deque(p2)
-p2_r.rotate(b - 1)
-p2_r = list(p2_r)
-print(p2_r)
-
-i = 0
-for el in p2_r:
-    if el in remaining:
-        c1[i] = el
-        if i == a - 1:
-            i = b + 1
-        else:
-            i += 1
-
+c1 = crossover(p1, p2)
 print("p1: ", p1)
 print("p2: ", p2)
 print("c1: ", c1)
